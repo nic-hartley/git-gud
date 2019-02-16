858d2cb88c5011e4baefbf9f2594d5897e5662cc
Michael-Bianconi <michael.a.bianconi@gmail.com>
2019-02-16 15:56:57 -0500

    Adding a Commit to the tree now returns the Commit

--- src/GitTree.cpp
+++ src/GitTree.cpp
@@ -66,15 +66,17 @@ int GitTree::getNumCommits() const
 	return this->commits.size();
 }
 
-void GitTree::addCommit()
+std::shared_ptr<Commit> GitTree::addCommit()
 {
 	auto commit = std::make_shared<Commit>(generateBranchID());
 
 	this->commits.push_back(commit);
 	this->numBranches++;
+
+	return commit;
 }
 
-void GitTree::addCommit(int parentID)
+std::shared_ptr<Commit> GitTree::addCommit(int parentID)
 {
 	auto parent = getCommit(parentID);
 	auto commit = std::make_shared<Commit>(parent->getBranch());
@@ -83,9 +85,11 @@ void GitTree::addCommit(int parentID)
 	parent->addChild(commit);
 
 	this->commits.push_back(commit);
+
+	return commit;
 }
 
-void GitTree::addCommitNewBranch(int parentID)
+std::shared_ptr<Commit> GitTree::addCommitNewBranch(int parentID)
 {
 	auto parent = getCommit(parentID);
 	auto commit = std::make_shared<Commit>(generateBranchID());
@@ -95,6 +99,8 @@ void GitTree::addCommitNewBranch(int parentID)
 
 	this->commits.push_back(commit);
 	this->numBranches++;
+
+	return commit;
 }
 
 void GitTree::undo()
