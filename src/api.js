751cc588dc4d69b1a8ef8f8000416ae975f5fc4f
Michael-Bianconi <michael.a.bianconi@gmail.com>
2019-02-17 09:23:35 -0500

    AMBER!

--- src/api.js
+++ src/api.js
@@ -4,6 +4,7 @@ const VERT_MARGIN = CIRCLE_RADIUS / 2;
 const COL_WIDTH = CIRCLE_RADIUS * 2 + HORIZ_MARGIN * 2;
 const ROW_HEIGHT = CIRCLE_RADIUS * 2 + VERT_MARGIN * 2;
 const LINE_ARC_RADIUS = VERT_MARGIN/2;
+const AMBER = "#FFB000";
 
 let dragging = false;
 let lastMouseX;
