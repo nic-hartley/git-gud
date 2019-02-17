036d09d11db2fd8b0665fae8c5d1768f1be5c4f0
Raghul Krishnan <raghulk@users.noreply.github.com>
2019-02-16 23:07:08 -0500

    cmd line with invalid command detection

--- src/api.js
+++ src/api.js
@@ -28,3 +28,6 @@ function branch(name) {
 function merge(into, from) {
   console.log("would merge", from, "into", into);
 }
+function checkout(into, from){
+    console.log("would checkout from", from, "into", into);
+}
